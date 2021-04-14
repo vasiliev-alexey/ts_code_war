a6af727d36b6be185dd8cd164069c79d20dc1498
vasiliev-alexey <alex_v81@MAIL.RU>
2021-04-14 16:45:24 +0300

    fix

---
+++ kata3/kata.test.ts
@@ -0,0 +1,18 @@
+import {G964} from './kata'
+
+describe("test", () => {
+
+
+    describe('test kata 1', () => {
+
+
+        it("Basic tests longest", function () {
+            expect(G964.longest("aretheyhere", "yestheyarehere")).toEqual("aehrsty");
+            expect(G964.longest("loopingisfunbutdangerous", "lessdangerousthancoding")).toEqual("abcdefghilnoprstu");
+            expect(G964.longest("inmanylanguages", "theresapairoffunctions")).toEqual("acefghilmnoprstuy");
+            expect(G964.longest("lordsofthefallen", "gamekult")).toEqual("adefghklmnorstu");
+        });
+    });
+
+
+})
