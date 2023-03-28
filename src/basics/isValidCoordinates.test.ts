79c8291f9c0a1ab6200c260e28e2c710085833d9
vasiliev-alexey <alex_v81@MAIL.RU>
2023-03-28 15:57:04 +0300

    kata
    Signed-off-by: vasiliev-alexey <alex_v81@MAIL.RU>

---
+++ src/basics/isValidCoordinates.test.ts
@@ -0,0 +1,65 @@
+const isValidCoordinates = (coordinates) => {
+    const isValid = (coordinate, type) => {
+        if (type === "lat") {
+            return Math.abs(coordinate) >= 0 && Math.abs(coordinate) <= 90;
+        } else {
+            return Math.abs(coordinate) >= 0 && Math.abs(coordinate) <= 180;
+        }
+    };
+
+    const isDigitOrSpecChar = (str) => {
+        const allowed = [
+            "-", ",", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        ];
+        for (let char of [...str]) {
+            if (!allowed.includes(char) || char === " ") {
+                return false;
+            }
+        }
+        return true;
+    };
+
+
+    const lat = coordinates.split(",")[0];
+    const long = coordinates.split(",")[1].slice(1);
+    return coordinates.split(",").length > 2 ||
+    !isValid(lat, "lat") ||
+    !isValid(long, "long") ||
+    !isDigitOrSpecChar(lat) ||
+    !isDigitOrSpecChar(long)
+        ? false
+        : true;
+};
+
+
+
+
+describe("Tests", () => {
+    it("test", () => {
+
+        const ValidCoordinates = [
+            "-23, 25",
+            "4, -3",
+            "24.53525235, 23.45235",
+            "04, -23.234235",
+            "43.91343345, 143"
+        ];
+        for (let i = 0; i < ValidCoordinates.length; i++) {
+            expect(isValidCoordinates(ValidCoordinates[i])).toBeTruthy();
+        }
+
+        const InvalidCoordinates = [
+            "23.234, - 23.4234",
+            "2342.43536, 34.324236",
+            "N23.43345, E32.6457",
+            "99.234, 12.324",
+            "6.325624, 43.34345.345",
+            "0, 1,2",
+            "0.342q0832, 1.2324",
+            "23.245, 1e1"
+        ];
+        for (let i in InvalidCoordinates) {
+            expect(isValidCoordinates(InvalidCoordinates[i])).toBeFalsy();
+        }
+    });
+});
